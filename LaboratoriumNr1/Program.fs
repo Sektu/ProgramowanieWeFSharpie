4aba9f098ee327116e4f89752a6b19eaff0ad984
Sektu <sirrotto@gmail.com>
2016-10-29 19:53:04 +0200

    added some initial code for first exercise

--- LaboratoriumNr1/Program.fs
+++ LaboratoriumNr1/Program.fs
@@ -1,7 +1,12 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿//6. Utworzyć funkcję wczytującą liczby wprowadzane z klawiatury, aż do napotkania zera. Jako
+//wyjście programu funkcja powinna zwrócić sumę wprowadzonych liczb oraz ich średnią
+//geometryczną. Wskazówka: Można posłużyć się funkcją pomocniczą.
+
+open System;
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let input = Console.ReadLine()
+    printfn "%A" input
+    Console.ReadLine() |> ignore
+    0
