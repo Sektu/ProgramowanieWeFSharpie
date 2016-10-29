632526fdf3b2b766f28d476ed6a6c4c2fe6b4f50
Sektu <sirrotto@gmail.com>
2016-10-29 20:20:03 +0200

    first exercise almost works, it needs dividing 1/count and finished

--- LaboratoriumNr1/Program.fs
+++ LaboratoriumNr1/Program.fs
@@ -4,9 +4,16 @@
 
 open System;
 
+let rec calculateSumOfInputNumbers (sum, product) count = 
+    let inputInt = Int32.Parse( Console.ReadLine() )
+    let inputDouble = Convert.ToDouble(inputInt)
+    if inputInt <> 0 then calculateSumOfInputNumbers ((sum + inputInt), (product * inputDouble)) (count + 1.0)
+    else (sum, product ** count)
+
 [<EntryPoint>]
 let main argv = 
-    let input = Console.ReadLine()
-    printfn "%A" input
+    let result = calculateSumOfInputNumbers (0, 1.0) 0.0
+    printfn "%A" result
+    //printfn "%A" (9.0 ** 0.5)
     Console.ReadLine() |> ignore
     0
